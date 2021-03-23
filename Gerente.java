213718b9dabd0a2aabdbfb805adcda4bd7339638
Ana Clara Luz <61888297+Ana-Luz@users.noreply.github.com>
2021-03-23 10:52:41 -0300

    Create Gerente.java

---
+++ Gerente.java
@@ -0,0 +1,15 @@
+
+public class Gerente extends Empregado {
+  
+  private double salario;
+  private double bonus;
+	
+	public void inserirSal(double salario) {
+    this.salario = salario;
+	}
+
+	public void inserirBonus(double bonus) {
+		this.bonus = bonus;
+	}
+
+}
