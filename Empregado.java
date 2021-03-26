8bdb698c56d599d7b2019b7062dde71f807adeab
vitor-tk <62676267+Vitor-TK@users.noreply.github.com>
2021-03-26 11:27:03 -0300

    Update Empregado.java

--- Empregado.java
+++ Empregado.java
@@ -1,9 +1,28 @@
+public class Vendedor extends Empregado implements Salario {
 
-public class Empregado extends Pessoa {
-	
-	protected String matricula;
-	
-	public void inserirMat(String matricula) {
-		this.matricula = matricula;
+	private double totalDasVendas;
+	private double comissao;
+
+	public double getTotalDasVendas() {
+		return totalDasVendas;
 	}
+
+	public void setTotalDasVendas(double totalDasVendas) {
+		this.totalDasVendas = totalDasVendas;
+	}
+
+	public double getComissao() {
+		return comissao;
+	}
+
+	public void setComissao(double comissao) {
+		this.comissao = comissao;
+	}
+
+	@Override
+	public double calcularSalario(double totalDeVendas, double comissao) {
+		double aux = totalDeVendas * comissao;
+		return aux;
+	}
+
 }
